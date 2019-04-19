1483699d61143d60e60bf7300c52ef01ed9832c4
nima.psybient@gmail.com <seenner.oracle@gmail.com>
2019-04-20 00:24:43 +0430

    Add a test marko entry Also Edit webpack config base on: [This starter](https://github.com/francosioquim/markojs-webpack-starter)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('offline-plugin/runtime').install();
 
-import './fonts/libre-baskerville-v5-latin-regular.woff';
-import './fonts/libre-baskerville-v5-latin-regular.woff2';
+// import './fonts/libre-baskerville-v5-latin-regular.woff';
+// import './fonts/libre-baskerville-v5-latin-regular.woff2';
 
-import './index.html';
-import './index.scss';
-import './scripts/script.js';
+// import './index.html';
+// import './index.scss';
+// import './scripts/script.js';
+
+var template = require('./template.marko')
+var html = template.renderToString({ name: 'Frank' });
